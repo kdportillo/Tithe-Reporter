d16cadf23529fbb6124398f3b5a3a58c67754acb
kdportillo <kportillo01@gmail.com>
2018-07-07 17:15:33 -0500

    add file search functionality, need to print to GUI

--- lib/gui.py
+++ lib/gui.py
@@ -1,18 +1,25 @@
-from Tkinter import Label, Button
+from Tkinter import Tk, Label, Button, Text, DISABLED, Message
+import tkFileDialog as fd
+import os
 
 class GUI:
     def __init__(self, master):
         self.master = master
         master.title("Tithe Reporter")
+        master.geometry('1000x500')
 
-        self.label = Label(master, text="LABEL 1")
+        self.label = Label(master, text="File Search")
         self.label.pack()
 
-        self.greet_button = Button(master, text="Greet", command=self.greet)
+        self.greet_button = Button(master, text="Choose a file", command=self.filesearch)
         self.greet_button.pack()
 
         self.close_button = Button(master, text="Close", command=master.quit)
         self.close_button.pack()
 
-    def greet(self):
-        print("Greetings")
+    def filesearch(self):
+        tmpdir = fd.askopenfilename(initialdir=os.getcwd(), title='Choose file')
+        if len(tmpdir) > 0:
+            return True
+        else:
+            return False
--- src/tithereporter.py
+++ src/tithereporter.py
@@ -3,7 +3,8 @@ from Tkinter import Tk
 sys.path.insert(0, '../lib/')
 import gui
 
-root = Tk()
-window = gui.GUI(root)
-root.mainloop()
+if __name__ == '__main__':
+    root = Tk()
+    window = gui.GUI(root)
+    root.mainloop()
 
