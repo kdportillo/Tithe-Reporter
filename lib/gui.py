5be80ad0a3b428dff6143a34134da87e567c9ed4
kdportillo <kportillo01@gmail.com>
2018-07-07 23:30:14 -0500

    change color of button if file found

--- lib/gui.py
+++ lib/gui.py
@@ -1,5 +1,6 @@
-from Tkinter import Tk, Label, Button, Text, DISABLED, Message
+from Tkinter import Tk, Label, Button, Text, DISABLED
 import tkFileDialog as fd
+import tkMessageBox as mb
 import os
 
 class GUI:
@@ -8,18 +9,22 @@ class GUI:
         master.title("Tithe Reporter")
         master.geometry('1000x500')
 
+        self.fileFound = False
+
         self.label = Label(master, text="File Search")
         self.label.pack()
 
-        self.greet_button = Button(master, text="Choose a file", command=self.filesearch)
-        self.greet_button.pack()
+        self.file_button = Button(master, text="Choose a file", command=self.filesearch)
+        self.file_button.pack()
 
         self.close_button = Button(master, text="Close", command=master.quit)
         self.close_button.pack()
 
     def filesearch(self):
         tmpdir = fd.askopenfilename(initialdir=os.getcwd(), title='Choose file')
-        if len(tmpdir) > 0:
-            return True
-        else:
-            return False
+        rtn = False
+        if tmpdir:
+            try:
+                self.file_button.configure(text='File Found', bg='green')
+            except:
+                mb.showerror("Open source file", "Failed to read file" + tmpdir)
